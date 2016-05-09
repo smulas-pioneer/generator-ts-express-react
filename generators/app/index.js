fe296653149aad8730db7b71d0413acdf39afa2f
Simone Mulas <simone.mulas@gmail.com>
2016-05-09 12:04:43 +0200

    prompt application name

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,19 +4,30 @@ var _ = require('lodash');
 module.exports = generators.Base.extend({
     constructor: function() {
         generators.Base.apply(this,arguments);
-            // This makes `appname` a required argument.
-            this.argument('appname', { type: String, required: true });
+//            this.argument('appname', { type: String, required: true });
             // And you can then access it later on this way; e.g. CamelCased
            // this.appname = _.camelCase(this.appname);  
     },
+    prompting: function () {
+        return this.prompt({
+            type    : 'input',
+            name    : 'name',
+            message : 'Your project name:'
+        }).then(function (answers) {
+            this.appname = answers.name;
+            this.log(answers.name);
+        }.bind(this));
+    },
     
-    test: function () {
+    elab: function () {
+        //Change Dest
         this.destinationRoot(this.appname);
         
+        //Copy Files
         this.fs.copyTpl(
-        this.templatePath(),
-        this.destinationPath(),
-            { title: this.appname }
-        );
+            this.templatePath(),
+             this.destinationPath(),
+                { title: this.appname }
+            );
     }
 });
