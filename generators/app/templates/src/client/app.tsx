9e02d3bce613cd4084c326c19174ba706fc530fc
Simone Mulas <simone.mulas@gmail.com>
2016-05-25 18:10:18 +0200

    changed versions - removed jquery

--- generators/app/templates/src/client/app.tsx
+++ generators/app/templates/src/client/app.tsx
@@ -3,7 +3,6 @@ import * as ReactDOM from 'react-dom';
 import * as React from 'react';
 import * as bootstrap from 'bootstrap'; // just to link the js
 import * as BS from 'react-bootstrap';
-import * as $ from 'jquery';
 
 import {HelloWorld} from './components/HelloWorld';
 
